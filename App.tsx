afc4a17a7ab1f638f80eb232a1a50e05aebf40d0
iamshujasaqib <iamshujasaqib@gmail.com>
2023-06-07 04:10:35 +0500

    base

---
+++ App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {MainApp} from './src/boot/MainApp';
+
+const App = () => {
+  return <MainApp />;
+};
+
+export default App;
---
+++ src/boot/MainApp.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+export const MainApp = () => {
+  return (
+    <>
+      <Text>Main App</Text>
+    </>
+  );
+};
